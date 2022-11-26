3bc8d4743c623815a2d202cf366409a47fb02cc4
shrsyc <99862109+shrsyc@users.noreply.github.com>
2022-11-26 21:52:44 +0530

    Update my.java

--- my.java
+++ my.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class my{
     public static void main(String[] args){
-        System.out.println("HI");
+        int a=10,b=15,c;
+        c=a+b;
+        System.out.println(c);
     }
 }
